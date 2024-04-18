94459ffc772c065a352749cbb5ae66c686bad972
我是海军呀 <31569037+player4sir@users.noreply.github.com>
2024-04-19 02:32:28 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,7 +50,7 @@ export default defineNuxtConfig({
         vueI18n: './i18n.config.ts',
         strategy: 'no_prefix',
         defaultLocale: 'zh',
-    }
+    },
     nitro: {
         vercel: {
             regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"]
